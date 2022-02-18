c6b56edf5c9c121ca9bc27f8c23719085bc185d4
Gabriel Ovalle <gabriel.ovalle@exelaration.com>
2022-02-18 12:00:31 -0500

    Can now select countries and states

--- main/javascript/main.js
+++ main/javascript/main.js
@@ -1,6 +1,57 @@
 let countriesURL = 'https://xc-countries-api.herokuapp.com/api/countries/';
-let statesURL = '';
-// fetch call
+
+function populateDropdown(inputArray, elementName, nameColumn, valueColumn){
+    inputArray.sort(function (a, b){
+        console.log(a);
+        console.log(b);
+        if (a.name > b.name){
+            return 1;
+        }
+        if (b.name > a.name ){
+            return -1;
+        }
+        return 0;
+    })
+    for (let i = 0; i < inputArray.length; i++){
+        var select = document.getElementById(elementName);
+        var option = document.createElement('option');
+        option.innerHTML = inputArray[i][nameColumn]
+        option.value = inputArray[i][valueColumn]
+        select.appendChild(option);
+    }
+}
+
 fetch(countriesURL)
-    .then((response) => {console.log(response.json());})
-    .then((data) => {console.log(data);})
+    .then((response) => response.json())
+    .then((data) => {  
+        populateDropdown(data, "dropdownCountry", "name", "code")
+        populateDropdown(data, "parentCountry", "name", "id")
+    })
+
+function openDropdownStates(){
+    let select = document.getElementById("dropdownCountry");
+    let countryCode = select.value;
+    let statesURL = 'https://xc-countries-api.herokuapp.com/api/countries/' + countryCode + '/states/';
+    fetch(statesURL)
+    .then((response) => response.json())
+    .then((data) => {
+        var select = document.getElementById("dropdownStates");
+        while (select.firstChild){
+            select.removeChild(select.firstChild);
+        }
+        populateDropdown(data, "dropdownStates", "name", "code")
+    })
+}
+
+function addNewCountry(){
+    fetch(countriesURL)
+    .then((response) => response.json())
+    .then((data) => {        
+        let newCountryID = data.length;
+        let newCountryName = document.getElementById("countryName").value;
+        let newCountryCode = document.getElementById("countryCode").value;
+        let newCountryEntry = '{"id":' + newCountryID + ',"code":"'+ newCountryCode + '","name":"' + newCountryName + '"}'
+        console.log(newCountryEntry);
+    })
+    
+}
